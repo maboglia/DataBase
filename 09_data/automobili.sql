020c00a24c27656cc57cffa241ee91a5ad167a3f
maboglia <mauro.bogliaccino@gmail.com>
2024-12-16 09:40:45 +0100

    agg

--- 09_data/automobili.sql
+++ 09_data/automobili.sql
@@ -30,3 +30,26 @@ INSERT INTO automobili (marca, modello, cilindrata, posti, prezzo) VALUES
 ('Chevrolet', 'Spark', 1200, 4, 11000.00),
 ('Suzuki', 'Swift', 1300, 5, 14000.00),
 ('Subaru', 'Outback', 2500, 5, 35000.00);
+
+-- Inserimento di altre 20 righe di dati fake con marche ripetute e modelli diversi
+INSERT INTO automobili (marca, modello, cilindrata, posti, prezzo) VALUES
+('Fiat', 'Tipo', 1400, 5, 18000.00),
+('Ford', 'Kuga', 2000, 5, 30000.00),
+('Volkswagen', 'Passat', 2000, 5, 35000.00),
+('Toyota', 'Corolla', 1500, 5, 20000.00),
+('BMW', 'Serie 1', 1500, 5, 28000.00),
+('Mercedes', 'GLA', 2000, 5, 38000.00),
+('Audi', 'Q3', 1600, 5, 34000.00),
+('Renault', 'Captur', 1300, 5, 21000.00),
+('Peugeot', '3008', 1600, 5, 29000.00),
+('Kia', 'Ceed', 1500, 5, 20000.00),
+('Hyundai', 'i20', 1200, 5, 15000.00),
+('Jeep', 'Compass', 1600, 5, 30000.00),
+('Honda', 'HR-V', 1800, 5, 26000.00),
+('Nissan', 'X-Trail', 2000, 5, 33000.00),
+('Mazda', 'Mazda3', 1500, 5, 23000.00),
+('Volvo', 'XC60', 2400, 5, 50000.00),
+('Tesla', 'Model Y', 0, 5, 55000.00),
+('Chevrolet', 'Trax', 1400, 5, 17000.00),
+('Suzuki', 'Vitara', 1600, 5, 20000.00),
+('Subaru', 'Forester', 2000, 5, 34000.00);
